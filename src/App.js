31deb11f481e5e99a55e6dbee39176ef6ca563ce
valigton <valigtonverissimo@outlook.com>
2019-08-08 10:21:11 -0300

    Bug de rotas consertado

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import EditarGenero from './EditarGenero'
 import axios from 'axios'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 
 const Home = () =>{
@@ -25,11 +26,13 @@ function App() {
     <Router>
       <div>
         <Header />
-        <Route path='/' exact component={Home} />
-        <Route path='/generos/novo' exact component={NovoGenero} />
-        <Route path='/generos/:id' exact component={EditarGenero} />
-        <Route path='/generos' exact component={Generos} />
-        <pre>{JSON.stringify(data)}</pre>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/generos' exact component={Generos} />
+          <Route path='/generos/:id' exact component={EditarGenero} />
+          <Route path='/generos/novo' exact component={NovoGenero} />
+          <pre>{JSON.stringify(data)}</pre>
+        </Switch>
       </div>
     </Router>
   );
